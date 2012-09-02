338eb9d2ef0b22a61c16f7176b11417207da8314
Karolin Varner <karo@cupdev.net>
2012-09-02 05:15:40 +0200

    Add support for generating keys

--- crack.cpp
+++ crack.cpp
@@ -1,5 +1,22 @@
 #include "cube.h"
+#include <stdio.h>
 
-int main(int argc, char** argv) {
+extern void genprivkey(const char *seed, vector<char> &privstr, vector<char> &pubstr);
+
+void genauthkey(const char *secret) {
+    if(!secret[0]) { 
+        printf("you must specify a secret password");
+        return;
+    }
+    
+    vector<char> privkey, pubkey;
+    genprivkey(secret, privkey, pubkey);
+    
+    printf("private key: %s\n", privkey.getbuf());
+    printf("public key: %s\n", pubkey.getbuf());
+}
+
+int main(const int argc, const char** argv) {
+    genauthkey(argv[0]); 
     return 0;
 }
